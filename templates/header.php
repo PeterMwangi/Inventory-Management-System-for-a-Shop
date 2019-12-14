e72bb0ba24dc590ec6e994014ea3b92646f85cd2
PeterMwangi <petermwangipro@gmail.com>
2019-12-14 22:23:45 +0300

    HTML files that contain the header.php, products, category and brand forms

---
+++ templates/header.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a class="navbar-brand" href="#">Inventory MS</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#"><i class="fa fa-home">&nbsp; &nbsp;</i>Home <span class="sr-only">(current)</span></a>
+            </li>
+
+
+          
+                <?php
+                if (isset($_SESSION["userid"])) {
+                    ?>
+                      <li class="nav-item active">
+                    <a class="nav-link" href="logout.php"><i class="fa fa-user">&nbsp; &nbsp;</i>Logout</a>
+
+                <?php
+                }
+
+                ?>
+
+
+            </li>
+
+
+
+
+        </ul>
+
+    </div>
+
+    <iframe style="float:right" src="http://free.timeanddate.com/clock/i6zqwstl/n170/tt0" frameborder="0" width="341" height="18"></iframe>
+
+
+</nav>
